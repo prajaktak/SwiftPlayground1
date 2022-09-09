399617dbddc7abb63b5fa1127dde93ae21e45e99
Prajakta <prachee.j@gmail.com>
2022-09-09 22:32:36 +0200

    committing playground for functions

---
+++ MyPlaygroundFunction.playground/Contents.swift
@@ -0,0 +1,11 @@
+//creating  functions
+func showMessage(number: Int, name: String){
+    print ("The Function call worked \(number) \(name)")
+}
+
+showMessage(number: 43, name: "myName")
+
+//function returning value
+func basicFunction() -> Bool{
+    return true
+}
